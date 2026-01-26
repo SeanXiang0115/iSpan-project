1751c043d68936ac45c50a072826c3f84c0546af
k85210 <kl85210@gmail.com>
2026-01-26 15:15:28 +0800

    config: 路由設定

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,8 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
+// 定義router
+const routes = [
+  {
+    
+  }
+];
+
+// 建立router
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: []
+});
 
-export default router
+export default router;
