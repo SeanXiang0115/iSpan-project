0fb5dc99f8795d743cf98066358689d266a397ff
SeanXiang0115 <a600sean@gmail.com>
2026-02-03 16:29:57 +0800

    fix: 調整訂位修改彈窗，尚未完成。

--- front-end/src/views/UserBookingsView.vue
+++ front-end/src/views/UserBookingsView.vue
@@ -1,5 +1,18 @@
 <script setup>
 import BaseButton from '@/components/common/BaseButton.vue';
+import Swal from 'sweetalert2';
+
+const updateBooking = (item) => {
+    // 以下是彈窗架構
+    Swal.fire({
+        icon: 'success',
+        title: '成功加入購物車',
+        text: `已選購  ${item.productName}`,
+        timer: 1500,
+        showConfirmButton: false
+    });
+}
+
 </script>
 
 <template>
@@ -7,7 +20,8 @@ import BaseButton from '@/components/common/BaseButton.vue';
     <h1 class="text-gdg mb-4">訂位查詢頁面(使用者)</h1>
 
     <div class="mb-4">
-      <BaseButton color="gdg">修改訂位</BaseButton>
+
+      <BaseButton color="gdg" @click="updateBooking">修改訂位</BaseButton>
     </div>
   </div>
 </template>
