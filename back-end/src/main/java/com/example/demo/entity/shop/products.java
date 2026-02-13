480fec8710f6628587186626e7d542147a818452
Anna <shan80519@gmail.com>
2026-02-13 16:33:31 +0800

    0213 new

---
+++ back-end/src/main/java/com/example/demo/entity/shop/products.java
@@ -0,0 +1,14 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
