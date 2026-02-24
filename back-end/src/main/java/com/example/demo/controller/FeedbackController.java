09f31ab7e0435caec17ce17e5cf810e5ce6b7fc9
merris3557 <merris3557@gmail.com>
2026-02-24 14:07:43 +0800

    Edit : FeedbackController_v1.0.1

--- back-end/src/main/java/com/example/demo/controller/FeedbackController.java
+++ back-end/src/main/java/com/example/demo/controller/FeedbackController.java
@@ -6,14 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.Feedback.FeedbackRequestDto;
+import com.example.demo.service.Feedback.FeedbackService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping
+@RequiredArgsConstructor
 public class FeedbackController {
+
+    private final FeedbackService feedbackService;
 
     @PostMapping("/feedback")
     public void createFeedback(@RequestBody FeedbackRequestDto dto) {
-
+        feedbackService.createFeedback(dto);
     }
 
 }
