0c5a649229fc9dc214846839a6ba0adbe2898435
SeanXiang0115 <a600sean@gmail.com>
2026-03-01 18:44:12 +0800

    update: 訂位相關dto, repo, service準備

--- back-end/src/main/java/com/example/demo/reservation/repository/BookingRepository.java
+++ back-end/src/main/java/com/example/demo/reservation/repository/BookingRepository.java
@@ -1,1 +1,9 @@
 package com.example.demo.reservation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.reservation.entity.Booking;
+
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
+
+}
--- back-end/src/main/java/com/example/demo/reservation/service/BookingService.java
+++ back-end/src/main/java/com/example/demo/reservation/service/BookingService.java
@@ -1,1 +1,26 @@
 package com.example.demo.reservation.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.reservation.dto.BookingResponseDto;
+import com.example.demo.store.repository.OpenHourRepository;
+import com.example.demo.store.repository.SeatRepository;
+import com.example.demo.store.repository.StoreInfoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BookingService {
+
+    private final StoreInfoRepository storeInfoRepository;
+    private final OpenHourRepository openHourRepository;
+    private final SeatRepository seatRepository;
+
+    // 前台專用：取得指定店家的預約設定
+    public BookingResponseDto getStoreReservationSettings(Integer storeId) {
+
+        return null;
+    }
+
+}
